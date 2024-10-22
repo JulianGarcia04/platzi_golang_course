74d17f1382b2c87f5e23d381e6678de223d65738
Julián Camilo García Rincón <jcgrrincon@gmail.com>
2024-10-22 05:34:18 -0500

    conditionals with Golang

--- src/main.go
+++ src/main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
-	"net/http"
-	"os"
 )
 
 // the size parameter must be in cm
@@ -103,6 +99,75 @@ func functions () {
 
 }
 
+func loops () {
+	// for loop
+	fmt.Println("for loop")
+
+	for i := 100; i >= 0; i-- {
+		fmt.Printf("número %d \n", i)
+	}
+
+	// for while loop
+	fmt.Println("For while loop")
+
+	counter := 100
+
+	for counter >= 0 {
+		fmt.Printf("el counter está en %d \n", counter);
+		counter--
+	}
+
+	// for forever loop
+	fmt.Println("For forever loop")
+
+	fmt.Println("Lo comenté para ahorar recursos")
+
+	// counterForever := 0
+
+	// for {
+	// 	fmt.Printf("el counter del for forever está en %d \n", counterForever)
+	// 	counterForever++
+	// }
+}
+
+func ifConditional (testValue int) {
+	nro := testValue
+	if nro >= 0 {
+		fmt.Println("Este numero está entre cero y más allá")
+		return
+	}
+
+	if nro < 0 {
+		fmt.Println("Este número es negativo")
+		return
+	}
+
+	fmt.Println("El número es nil o no se que es")
+}
+
+func switchConditional (nroTest int) {
+
+	switch nro := nroTest; nro  {
+	case 10:
+		fmt.Println("Es un número 10")
+	case 20:
+		fmt.Println("Es un número 20")
+	case -1:
+		fmt.Println("Es un número -1")
+	default:
+		fmt.Println("Puede ser un número")
+	}
+
+	switch nro := nroTest; {
+	case nro >= 0:
+		fmt.Println("Este numero está entre cero y más allá")
+	case nro < 0: 
+		fmt.Println("Este número es negativo")
+	default:
+		fmt.Println("Es un número ???")
+	}
+}
+
 func main () {
 	fmt.Println("-------------hello world------------------")
 	fmt.Println("Hello world!!")
@@ -113,24 +178,36 @@ func main () {
 	fmt.Println("-------------print features-----------------------")
 	printFeatures()
 
-	fmt.Println("-------------functions----------------------")
+	fmt.Println("-------------functions-----------------")
 	functions()
+
+	fmt.Println("-------------loops---------------")
+	loops()
+
+	fmt.Println("-------------if conditional---------------")
+	ifConditional(0)
+	ifConditional(-2)
+
+	fmt.Println("-------------switch conditional-------------")
+	switchConditional(10)
+	switchConditional(20)
+	switchConditional(-1)
 
 	// my first server in go :)
 	// this it has the course but I wanted create a server
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
-	}
+	// helloHandler := func(w http.ResponseWriter, req *http.Request) {
+	// 	io.WriteString(w, "Hello, world!\n")
+	// }
 
-	http.HandleFunc("/hello", helloHandler)
+	// http.HandleFunc("/hello", helloHandler)
 
-	err := http.ListenAndServe(":3000", nil)
+	// err := http.ListenAndServe(":3000", nil)
 
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("Server closed\n")
-	} else if err != nil {
-		fmt.Printf("error stating server: %s\n", err);
-		os.Exit(1)
-	}
+	// if errors.Is(err, http.ErrServerClosed) {
+	// 	fmt.Printf("Server closed\n")
+	// } else if err != nil {
+	// 	fmt.Printf("error stating server: %s\n", err);
+	// 	os.Exit(1)
+	// }
 }
